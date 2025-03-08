78f528a1ef963a08096cd633e5c807c897a6a67e
Muhy Eddin Za'ter <muhizatar95@gmail.com>
2025-03-08 15:44:42 -0700

    Add components directory

---
+++ components/AvrModel.jl
@@ -0,0 +1,5 @@
+```
+This is the code for modeling the AVR EXST1.
+Figure available in https://www.powerworld.com/WebHelp/Content/TransientModels_HTML/Exciter%20EXST1_GE.htm
+
+```
---
+++ components/SynchronousMachineModel.jl
@@ -0,0 +1,3 @@
+```
+This code is for the Synchronous Machine Sauer Pai 6 state model.
+```
