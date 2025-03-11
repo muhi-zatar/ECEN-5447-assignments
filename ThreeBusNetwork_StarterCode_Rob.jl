425e729cea99c9112cede52e3c78be6e331d422a
Rob Stallman <robstallman3@gmail.com>
2025-03-11 16:07:10 -0600

    Initial network model - Does not initialize correctly

--- ThreeBusNetwork_StarterCode_Rob.jl
+++ ThreeBusNetwork_StarterCode_Rob.jl
@@ -40,19 +40,40 @@ Q = pf_result["bus_results"].Q_net / PSY.get_base_power(sys) # [pu(MVar)]
 # Section 2: Calculate relevant quantities needed for running a time domain simulation
 # -----------------------------------------------------------------------------------------
 
-##### TODO: Using v,θ,P,Q to find the parameters and initial conditions
-
-# To get you started, here are the branch parameters
-PSY.show_components(Line, sys, [:r, :x, :b, :arc])
-
 # Find complex power
-S = P + im * Q
+S = complex.(P, Q)
 
-# Find complex voltage
-V = v .* (cos.(θ) .+ im .* sin.(θ))
+# Find complex voltage in ABC reference frame
+V_abc = v .* ℯ .^ (im .* θ)
+
+##### MOVE TO NETWORK DQ REFERENCE FRAME #####
+# Find DQ voltage (Milano Eigenvalue Problems Eqn 1.44)
+v_d = -v .* sin.(θ)             # Direct-axis component of bus voltage
+v_q = v .* cos.(θ)              # Quadrature-axis component of bus voltage
+V_dq = complex.(v_d, v_q)       # Complex bus voltage in network DQ reference frame
+
+# Sanity check
+V_test = V_dq .* ℯ .^ (-im * π / 2)
+if norm(V_test - V_abc, Inf) > 1e-10
+    throw("DQ voltage calculation is probably wrong. Difference between calculated and expected: $(norm(V_test - V_abc,Inf))")
+else
+    println("DQ voltage calculation looks good. Difference between calculated and expected: $(norm(V_test - V_abc,Inf))")
+end
 
 # Find complex current
-I = conj(S ./ V)
+I_dq = conj(S ./ V_dq)          # Complex bus current injections in network DQ reference frame
+i_d = real(I_dq)                # Direct-axis component of bus current injections
+i_q = imag(I_dq)                # Quadrature-axis component of bus current injections
+
+# Sanity check
+P_test = (v_d .* i_d) .+ (v_q .* i_q)
+Q_test = (v_q .* i_d) .- (v_d .* i_q)
+S_test = complex.(P_test, Q_test)
+if norm(S_test - S, Inf) > 1e-10
+    throw("DQ current calculation is probably wrong. Difference between calculated and expected apparent power: $(norm(S_test - S,Inf))")
+else
+    println("DQ current calculation looks good. Difference between calculated and expected apparent power: $(norm(S_test - S,Inf))")
+end
 
 # Define some arbitrary generator reactances
 x1 = 1.0
@@ -67,32 +88,6 @@ line_1_2 = get_component(Line, sys, "BUS 1-BUS 2-i_1")
 line_1_3 = get_component(Line, sys, "BUS 1-BUS 3-i_1")
 line_2_3 = get_component(Line, sys, "BUS 2-BUS 3-i_1")
 
-# Define state vector elements
-i_12_d = 0
-i_12_q = 0
-i_13_d = 0
-i_13_q = 0
-i_23_d = 0
-i_23_q = 0
-v_1_d = real(V)[1]
-v_1_q = imag(V)[1]
-v_2_d = real(V)[2]
-v_2_q = imag(V)[1]
-v_3_d = real(V)[3]
-v_3_q = imag(V)[1]
-i_b12_d_1 = 0
-i_b12_q_1 = 0
-i_b12_d_2 = 0
-i_b12_q_2 = 0
-i_b13_d_1 = 0
-i_b13_q_1 = 0
-i_b13_d_3 = 0
-i_b13_q_3 = 0
-i_b23_d_2 = 0
-i_b23_q_2 = 0
-i_b23_d_3 = 0
-i_b23_q_3 = 0
-
 # Define parameter vector elements
 R_12 = get_r(line_1_2)
 X_12 = get_x(line_1_2)
@@ -103,12 +98,57 @@ B_13 = get_b(line_1_3)[1] * 2
 R_23 = get_r(line_2_3)
 X_23 = get_x(line_2_3)
 B_23 = get_b(line_2_3)[1] * 2
-i_1_d = real(I)[1]
-i_1_q = imag(I)[1]
-i_2_d = real(I)[2]
-i_2_q = imag(I)[2]
-i_3_d = real(I)[3]
-i_3_q = imag(I)[3]
+i_1_d = i_d[1]
+i_1_q = i_q[1]
+i_2_d = i_d[2]
+i_2_q = i_q[2]
+i_3_d = i_d[3]
+i_3_q = i_q[3]
+
+# Define state vector elements (Assume steady-state)
+v_1_d = v_d[1]                                      # Bus 1 d-axis terminal voltage             
+v_1_q = v_q[1]                                      # Bus 1 q-axis terminal voltage             
+v_2_d = v_d[2]                                      # Bus 2 d-axis terminal voltage             
+v_2_q = v_q[2]                                      # Bus 2 q-axis terminal voltage             
+v_3_d = v_d[3]                                      # Bus 3 d-axis terminal voltage             
+v_3_q = v_q[3]                                      # Bus 3 q-axis terminal voltage  
+i_b12_d_1 = -1 * (B_12 / 2) * v_1_q                 # Line 1-2 d-axis shunt current at bus 1
+i_b12_q_1 = (B_12 / 2) * v_1_d                      # Line 1-2 q-axis shunt current at bus 1
+i_b12_d_2 = -1 * (B_12 / 2) * v_2_q                 # Line 1-2 d-axis shunt current at bus 2
+i_b12_q_2 = (B_12 / 2) * v_2_d                      # Line 1-2 q-axis shunt current at bus 2
+i_b13_d_1 = -1 * (B_13 / 2) * v_1_q                 # Line 1-3 d-axis shunt current at bus 1
+i_b13_q_1 = (B_13 / 2) * v_1_d                      # Line 1-3 q-axis shunt current at bus 1
+i_b13_d_3 = -1 * (B_13 / 2) * v_3_q                 # Line 1-3 d-axis shunt current at bus 3
+i_b13_q_3 = (B_13 / 2) * v_3_d                      # Line 1-3 q-axis shunt current at bus 3
+i_b23_d_2 = -1 * (B_23 / 2) * v_2_q                 # Line 2-3 d-axis shunt current at bus 2
+i_b23_q_2 = (B_23 / 2) * v_1_d                      # Line 2-3 q-axis shunt current at bus 2
+i_b23_d_3 = -1 * (B_23 / 2) * v_3_q                 # Line 2-3 d-axis shunt current at bus 3
+i_b23_q_3 = (B_23 / 2) * v_3_d                      # Line 2-3 q-axis shunt current at bus 3
+
+# Still working on this – using the pseudoinverse for now because the A matrix is singular
+A = [1 1 0; 1 0 -1; 0 1 1]
+b = [i_1_d - (i_b12_d_1 + i_b13_d_1); (i_b12_d_2 + i_b23_d_2) - i_2_d; (i_b13_d_3 + i_b23_d_3) - i_3_d]
+(i_12_d, i_13_d, i_23_d) = pinv(A) * b
+
+A = [1 1 0; 1 0 -1; 0 1 1]
+b = [i_1_q - (i_b12_q_1 + i_b13_q_1); (i_b12_q_2 + i_b23_q_2) - i_2_q; (i_b13_q_3 + i_b23_q_3) - i_3_q]
+(i_12_q, i_13_q, i_23_q) = pinv(A) * b
+
+# Sanity check
+res_i = [
+    i_1_d - i_12_d - i_13_d - i_b12_d_1 - i_b13_d_1;
+    i_1_q - i_12_q - i_13_q - i_b12_q_1 - i_b13_q_1;
+    i_2_d + i_12_d - i_23_d - i_b12_d_2 - i_b23_d_2;
+    i_2_q + i_12_q - i_23_q - i_b12_q_2 - i_b23_q_2;
+    i_3_d + i_23_d + i_13_d - i_b13_d_3 - i_b23_d_3;
+    i_3_q + i_23_q + i_13_q - i_b13_q_3 - i_b23_q_3
+]
+
+if norm(res_i, Inf) > 1e-10
+    @warn "DQ current calculation is probably wrong. Unaccounted for current: $(norm(res_i,Inf))"
+else
+    println("DQ current calculation looks good. Unaccounted for current: $(norm(res_i - S,Inf))")
+end
 
 # -----------------------------------------------------------------------------------------
 # Section 3: Build ODEProblem
@@ -116,11 +156,51 @@ i_3_q = imag(I)[3]
 
 # Define function to be used by integration method 
 function three_bus_network(du, u, p, t)
-    ##### TODO: Write the system of equations 
+    i_12_d, i_12_q, i_13_d, i_13_q, i_23_d, i_23_q, v_1_d, v_1_q, v_2_d, v_2_q, v_3_d, v_3_q, i_b12_d_1, i_b12_q_1, i_b12_d_2, i_b12_q_2, i_b13_d_1, i_b13_q_1, i_b13_d_3, i_b13_q_3, i_b23_d_2, i_b23_q_2, i_b23_d_3, i_b23_q_3 = u
+    R_12, X_12, B_12, R_13, X_13, B_13, R_23, X_23, B_23, i_1_d, i_1_q, i_2_d, i_2_q, i_3_d, i_3_q = p
+
+    # Helper variables
+    B1_eq = (B_12 + B_13)
+    B2_eq = (B_12 + B_23)
+    B3_eq = (B_13 + B_23)
+
+    # Define equations in the same order as the u vector
+    # Line currents (differential)
+    du[1] = (v_1_d - v_2_d - R_12 * i_12_d + X_12 * i_12_q) / X_12          # d/dt (i_12_d)
+    du[2] = (v_1_q - v_2_q - R_12 * i_12_q - X_12 * i_12_d) / X_12          # d/dt (i_12_q)
+    du[3] = (v_1_d - v_3_d - R_13 * i_13_d + X_13 * i_13_q) / X_13          # d/dt (i_13_d)
+    du[4] = (v_1_q - v_3_q - R_13 * i_13_q - X_13 * i_13_d) / X_13          # d/dt (i_13_q)
+    du[5] = (v_2_d - v_3_d - R_23 * i_23_d + X_23 * i_23_q) / X_23          # d/dt (i_23_d)
+    du[6] = (v_2_q - v_3_q - R_23 * i_23_q - X_23 * i_23_d) / X_23          # d/dt (i_23_q)
+
+    # Bus voltages (differential)
+    du[7] = (B1_eq / 2) * v_1_q + i_b12_d_1 + i_b13_d_1                     # d/dt (v_1_d)
+    du[8] = -1 * (B1_eq / 2) * v_1_d + i_b12_q_1 + i_b13_q_1                # d/dt (v_1_q)
+    du[9] = (B2_eq / 2) * v_2_q + i_b12_d_2 + i_b23_d_2                     # d/dt (v_2_d)
+    du[10] = -1 * (B2_eq / 2) * v_2_d + i_b12_q_2 + i_b23_q_2               # d/dt (v_2_q)
+    du[11] = (B3_eq / 2) * v_3_q + i_b23_d_3 + i_b13_d_3                    # d/dt (v_3_d)
+    du[12] = -1 * (B3_eq / 2) * v_3_d + i_b23_q_3 + i_b13_q_3               # d/dt (v_3_q)
+
+    # Shunt currents (algebraic)
+    # Line 1-2
+    du[13] = i_1_d - i_12_d - i_13_d - i_b12_d_1 - i_b13_d_1                 # d/dt (i_b12_d_1) = 0
+    du[14] = i_1_q - i_12_q - i_13_q - i_b12_q_1 - i_b13_q_1                 # d/dt (i_b12_q_1) = 0
+    du[15] = i_2_d + i_12_d - i_23_d - i_b12_d_2 - i_b23_d_2                 # d/dt (i_b12_d_2) = 0
+    du[16] = i_2_q + i_12_q - i_23_q - i_b12_q_2 - i_b23_q_2                 # d/dt (i_b12_q_2) = 0
+    # Line 1-3
+    du[17] = i_1_d - i_12_d - i_13_d - i_b12_d_1 - i_b13_d_1                 # d/dt (i_b13_d_1) = 0
+    du[18] = i_1_q - i_12_q - i_13_q - i_b12_q_1 - i_b13_q_1                 # d/dt (i_b13_q_1) = 0
+    du[19] = i_3_d + i_23_d + i_13_d - i_b23_d_3 - i_b13_d_3                 # d/dt (i_b13_d_3) = 0
+    du[20] = i_3_q + i_23_q + i_13_q - i_b23_q_3 - i_b13_q_3                 # d/dt (i_b13_q_3) = 0
+    # Line 2-3
+    du[21] = i_2_d + i_12_d - i_23_d - i_b12_d_2 - i_b23_d_2                 # d/dt (i_b23_d_2) = 0
+    du[22] = i_2_q + i_12_q - i_23_q - i_b12_q_2 - i_b23_q_2                 # d/dt (i_b23_q_2) = 0
+    du[23] = i_3_d + i_23_d + i_13_d - i_b23_d_3 - i_b13_d_3                 # d/dt (i_b23_d_3) = 0
+    du[24] = i_3_q + i_23_q + i_13_q - i_b23_q_3 - i_b13_q_3                 # d/dt (i_b23_q_3) = 0
 end
 
 # Build the mass matrix
-M_diagonal = [] ##### TODO: Fill this with the coefficients of the derivatives 
+M_diagonal = [ones(12); zeros(12)]
 M = Diagonal(M_diagonal)  # creates diagonal square matrix from a vector
 
 # Build function 
@@ -131,15 +211,13 @@ tspan = (0.0, 0.2)
 
 # Build initial condition vector
 # Order will be:
-# [i_12_d, i_12_q, i_13_d, i_13_q, i_23_d, i_23_q, v_1_d, v_1_q, v_2_d, v_2_q, v_3_d, v_3_q, i_b12_d_1, i_b12_q_1, i_b12_d_2, i_b12_q_2, i_b13_d_1, i_b13_q_1, i_b13_d_3, i_b13_q_3, i_b23_d_2, i_b23_q_2, i_b23_d_3, i_b23_q_3]
-##### TODO: Fill this with initial conditions you calculated in Section 2
+#u0= [i_12_d, i_12_q, i_13_d, i_13_q, i_23_d, i_23_q, v_1_d, v_1_q, v_2_d, v_2_q, v_3_d, v_3_q, i_b12_d_1, i_b12_q_1, i_b12_d_2, i_b12_q_2, i_b13_d_1, i_b13_q_1, i_b13_d_3, i_b13_q_3, i_b23_d_2, i_b23_q_2, i_b23_d_3, i_b23_q_3]
 u0 = [i_12_d, i_12_q, i_13_d, i_13_q, i_23_d, i_23_q, v_1_d, v_1_q, v_2_d, v_2_q, v_3_d, v_3_q, i_b12_d_1, i_b12_q_1, i_b12_d_2, i_b12_q_2, i_b13_d_1, i_b13_q_1, i_b13_d_3, i_b13_q_3, i_b23_d_2, i_b23_q_2, i_b23_d_3, i_b23_q_3]
 
 
 # Build parameter vector
 # Order will be:
-# [R_12, X_12, B_12, R_13, X_13, B_13, R_23, X_23, B_23, i_1_d, i_1_q, i_2_d, i_2_q, i_3_d, i_3_q]
-##### TODO: Fill this with parameters you calculated in Section 2
+#p= [R_12, X_12, B_12, R_13, X_13, B_13, R_23, X_23, B_23, i_1_d, i_1_q, i_2_d, i_2_q, i_3_d, i_3_q]
 p = [R_12, X_12, B_12, R_13, X_13, B_13, R_23, X_23, B_23, i_1_d, i_1_q, i_2_d, i_2_q, i_3_d, i_3_q]
 
 # Check initial condition consistency
