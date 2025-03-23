db940258a49716da025654ac414ac360566f4b2d
Rob Stallman <robstallman3@gmail.com>
2025-03-23 11:15:08 -0600

    # Changes - Aux variables are not used in the update function anymore
    - More print statements for debugging
    
    - Cleaned up comments
    
    # Results
    - No changed results
    - Confirming that network + machine is still stable in steady-state (no perturbations)

--- deliverables/run_gen_net.jl
+++ deliverables/run_gen_net.jl
@@ -4,8 +4,8 @@ Pkg.activate("../.")
 
 using DifferentialEquations
 using Plots
+using Plots.PlotMeasures
 using LinearAlgebra
-#using Sundials
 
 include("NetworkModel.jl")
 include("SauerPaiMachineModel.jl")
@@ -22,6 +22,22 @@ const EQ_P = 3
 const ED_P = 4
 const PSI_D_PP = 5
 const PSI_Q_PP = 6
+const I_12_D_IDX = 1
+const I_12_Q_IDX = 2
+const I_13_D_IDX = 3
+const I_13_Q_IDX = 4
+const I_23_D_IDX = 5
+const I_23_Q_IDX = 6
+const V_1_D_IDX = 7
+const V_1_Q_IDX = 8
+const V_2_D_IDX = 9
+const V_2_Q_IDX = 10
+const V_3_D_IDX = 11
+const V_3_Q_IDX = 12
+const I_1_D_IDX = 13
+const I_1_Q_IDX = 14
+const I_3_D_IDX = 15
+const I_3_Q_IDX = 16
 
 mutable struct MachineNetworkParams
     network::ThreeBusNetwork
@@ -55,6 +71,15 @@ function run_machine_network(network_file)
     network_states, i_2_d_init, i_2_q_init = initialize_network(network, V_sol, θ_sol, P_sol, Q_sol)
     machine_states, Vf_init, τ_m_init = initialize_machine(machine, V_terminal, V_angle, P, Q)
 
+    # For debugging
+    V_terminal_init = (network_states[V_2_D_IDX] + im * network_states[V_2_Q_IDX]) * exp(-im * π / 2)
+    P_terminal_init = network_states[V_2_D_IDX] * i_2_d_init + network_states[V_2_Q_IDX] * i_2_q_init
+    Q_terminal_init = network_states[V_2_Q_IDX] * i_2_d_init - network_states[V_2_D_IDX] * i_2_q_init
+    S_terminal_init = complex(P_terminal_init, Q_terminal_init)
+    S_terminal = complex(P, Q) # From PF
+    sanity_check(V_terminal_init, V_terminal, "Initial quasi-static voltage")
+    sanity_check(S_terminal_init, S_terminal, "Initial apparent power")
+
     println("\nInitial States:")
     println("Machine states:")
     println("  Delta (rotor angle): $(machine_states[DELTA])")
@@ -66,6 +91,27 @@ function run_machine_network(network_file)
 
     println("Initial field voltage (Vf): $Vf_init")
     println("Initial mechanical torque (τm): $τ_m_init")
+
+    println("Network states:")
+    println("  I_12_D: $(network_states[I_12_D_IDX])")
+    println("  I_12_Q: $(network_states[I_12_Q_IDX])")
+    println("  I_13_D: $(network_states[I_13_D_IDX])")
+    println("  I_13_Q: $(network_states[I_13_Q_IDX])")
+    println("  I_23_D: $(network_states[I_23_D_IDX])")
+    println("  I_23_Q: $(network_states[I_23_Q_IDX])")
+    println("  V_1_D: $(network_states[V_1_D_IDX])")
+    println("  V_1_Q: $(network_states[V_1_Q_IDX])")
+    println("  V_2_D: $(network_states[V_2_D_IDX])")
+    println("  V_2_Q: $(network_states[V_2_Q_IDX])")
+    println("  V_3_D: $(network_states[V_3_D_IDX])")
+    println("  V_3_Q: $(network_states[V_3_Q_IDX])")
+    println("  I_1_D: $(network_states[I_1_D_IDX])")
+    println("  I_1_Q: $(network_states[I_1_Q_IDX])")
+    println("  I_3_D: $(network_states[I_3_D_IDX])")
+    println("  I_3_Q: $(network_states[I_3_Q_IDX])")
+
+    println("Initial bus power (from PF): $(complex(P,Q))")
+    println("Initial bus power (calculated): $(S_terminal_init)")
 
     states = vcat(network_states, machine_states)
 
@@ -82,21 +128,7 @@ function run_machine_network(network_file)
         machine_idx
     )
 
-    # M_system = zeros(Float64, length(states))
-
-    # if isa(network.M, Vector)
-    #     M_system[network_idx] .= network.M
-    # else
-    #     for i in 1:length(network_idx)
-    #         M_system[network_idx[i]] = network.M[i, i]
-    #     end
-    # end
-
-    # M_system[machine_idx] .= 1.0
-
-    # mass_matrix = Diagonal(M_system)
-
-    # Define auxiliary variabls
+    # Define auxiliary variables (FOR PLOTTING/DEBUGGING ONLY)
     V_terminal_aux = Complex{Float64}[]
     S_terminal_aux = Complex{Float64}[]
     t_aux = Float64[]
@@ -113,36 +145,32 @@ function run_machine_network(network_file)
         network_states = u[params.network_idx]
         machine_states = u[params.machine_idx]
 
-        # Make copies of states for Float64 compatibility
-        network_states_f64 = convert.(Float64, network_states)
-        machine_states_f64 = convert.(Float64, machine_states)
-
         # Arrays for derivatives
         du_network = similar(network_states, length(params.network_idx))
         du_machine = similar(machine_states, length(params.machine_idx))
 
+        # Calculate terminal voltage from current states (to use in update_machine_states!)
+        v_2_d = network_states[V_2_D_IDX]
+        v_2_q = network_states[V_2_Q_IDX]
+        V_terminal = (v_2_d + im * v_2_q) * exp(-im * π / 2)
+
+
         # Update the states of each component
-        I_terminal_machine_pos, S_terminal_machine, ω_machine, V_mag, I_mag = update_machine_states!(
-            machine_states_f64,
+        _, S_terminal_machine, _, _, _ = update_machine_states!(
+            machine_states,
             du_machine,
-            V_terminal_aux[end],
+            V_terminal,
             params.Vf,
             params.τm,
             params.machine
         )
 
-        V_terminal, S_terminal_network, I_terminal_network, i_2_d, i_2_q = update_network_states!(
-            network_states_f64,
+        _, _, _, i_2_d, i_2_q = update_network_states!(
+            network_states,
             du_network,
-            S_terminal_aux[end],
+            S_terminal_machine,
             params.network
         )
-
-        # These all throw errors when the system is perturbed, but checking S inside update_network_states! does not
-        # TODO: Check why
-        #sanity_check(S_terminal_network, S_terminal_machine, "Machine vs. Network Power", false)
-        #sanity_check(I_terminal_network, I_terminal_machine_pos, "Machine vs. Network Current", false)
-        #sanity_check(abs(V_terminal), V_mag, "Machine vs. Network Bus Voltage Magnitude", false)
 
         # Update auxiliary variables
         push!(V_terminal_aux, V_terminal)
@@ -157,25 +185,16 @@ function run_machine_network(network_file)
 
         #sanity_check(du, zeros(length(states)), "Derivatives", false)
 
-        # if (t > 4.98) & (t < 5.2)
-        #     if abs(t - round(t)) < 0.01
-        #         println("t=$t, Derivatives: $du")
-        #     end
-        # end
-
         if abs(t - round(t)) < 0.001
-            println("t=$t: δ=$(machine_states[DELTA]), ω=$ω_machine, τm=$(params.τm), Vf=$(params.Vf), V_mag=$V_mag, I_mag=$I_mag")
+            println("t=$t: δ=$(machine_states[DELTA]), ω=$(machine_states[OMEGA]), τm=$(params.τm), Vf=$(params.Vf), V_terminal=$V_terminal, S_terminal=$S_terminal_machine")
         end
     end
 
-    # # Build function 
-    # explicitDAE_M = ODEFunction(machine_network_dynamics!, mass_matrix=mass_matrix)
-
-    tspan = (0.0, 5.0)
+    tspan = (0.0, 25.0)
     prob = ODEProblem(machine_network_dynamics!, states, tspan, p)
 
     # Define the set of times to apply a perturbation
-    perturb_times = [2.0]
+    perturb_times = [40.0]
 
     # Define the condition for which to apply a perturbation
     function condition(u, t, integrator)
@@ -200,7 +219,7 @@ function run_machine_network(network_file)
     cb = DiscreteCallback(condition, affect!)
 
     # Run simulation
-    sol = solve(prob, Tsit5(), dt=0.00005, adaptive=false, saveat=0.01, callback=cb, tstops=perturb_times)
+    sol = solve(prob, Rodas5P(autodiff=false), saveat=0.01, callback=cb, tstops=perturb_times)
 
     t = sol.t
 
@@ -226,7 +245,7 @@ function run_machine_network(network_file)
     p5 = plot(sol, idxs=(0, [13, 14, 15, 16]), title="Network Bus Current Injections", labels=["I_1_D" "I_1_Q" "I_3_D" "I_3_Q"])
     plot!(p5, t_aux, i_2_d_aux, label="I_2_D")
     plot!(p5, t_aux, i_2_q_aux, label="I_2_Q")
-    p_combined = plot(p1, p2, p3, p4, p5, layout=(5, 1), size=(1200, 2000))
+    p_combined = plot(p1, p2, p3, p4, p5, layout=(5, 1), size=(1200, 2000), leftmargin=50mm)
     savefig(p_combined, "machine_network_results.png")
 
     return sol
