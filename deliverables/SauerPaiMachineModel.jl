c249be2ab0d6f68e802f0256d28dc7b90dc16fe8
Muhy Eddin Za'ter <Muhizatar95@gmail.com>
2025-04-02 11:43:29 -0600

    Add RI_DQ new transformation, fix gamma type, add intialization using NLsolve

--- deliverables/SauerPaiMachineModel.jl
+++ deliverables/SauerPaiMachineModel.jl
@@ -5,7 +5,7 @@ export SauerPaiMachine, initialize_machine, update_machine_states!
 
 using LinearAlgebra
 using DifferentialEquations
-
+using NLsolve
 # Defining the convention for each states
 const DELTA = 1
 const OMEGA = 2
@@ -13,19 +13,31 @@ const EQ_P = 3
 const ED_P = 4
 const PSI_D_PP = 5
 const PSI_Q_PP = 6
+const PSI_D = 7
+const PSI_Q = 8
+
+const STRICT_NLSOLVE_F_TOLERANCE = 1e-8
 
 # Helper functions
 # not all of them are used.
 
 # RI to DQ transformation matrix
-function ri_dq(delta)
-    return [cos(delta) sin(delta); -sin(delta) cos(delta)]
+function ri_dq_machine(δ::T) where {T<:Number}
+    #Uses the reference frame of the Kundur page 852 of RI to dq
+    return Float64[
+        sin(δ) -cos(δ)
+        cos(δ) sin(δ)
+    ]
 end
 
-# DQ to RI transformation matrix
-function dq_ri(delta)
-    return [cos(delta) -sin(delta); sin(delta) cos(delta)]
+function dq_ri_machine(δ::T) where {T<:Number}
+    ## Uses the referenceframe of the Kundur page 852 of dq to RI
+    return Float64[
+        sin(δ) cos(δ)
+        -cos(δ) sin(δ)
+    ]
 end
+
 
 # Machine structure for parameters
 mutable struct SauerPaiMachine
@@ -66,10 +78,10 @@ mutable struct SauerPaiMachine
         Tq0_p=0.85,
         Td0_pp=0.032,
         Tq0_pp=0.05,
-        γ_d1=(Xd_pp - Xl) / (Xd_p - Xl), # Removed "machine" as the object is not yet created.
-        γ_q1=(Xq_pp - Xl) / (Xq_p - Xl), # Removed "machine" as the object is not yet created.
-        γ_d2=(1 - γ_d1) / (Xd_p - Xl), # Removed "machine" as the object is not yet created.
-        γ_q2=(1 - γ_q1) / (Xq_p - Xl), # Removed "machine" as the object is not yet created.
+        γ_d1=0.1282051282051283,
+        γ_q1=0.7142857142857143,
+        γ_d2=22.35371466140696,
+        γ_q2=2.9154518950437316,
         H=3.148,
         D=2.0,
         base_power=100.0,
@@ -83,6 +95,23 @@ mutable struct SauerPaiMachine
             system_base_power, system_base_frequency, H, D)
     end
 end
+
+get_R(machine::SauerPaiMachine) = machine.R
+get_Xd(machine::SauerPaiMachine) = machine.X_d
+get_Xq(machine::SauerPaiMachine) = machine.X_q
+get_Xd_p(machine::SauerPaiMachine) = machine.Xd_p
+get_Xq_p(machine::SauerPaiMachine) = machine.Xq_p
+get_Xd_pp(machine::SauerPaiMachine) = machine.Xd_pp
+get_Xq_pp(machine::SauerPaiMachine) = machine.Xq_pp
+get_Xl(machine::SauerPaiMachine) = machine.Xl
+get_Td0_p(machine::SauerPaiMachine) = machine.Td0_p
+get_Tq0_p(machine::SauerPaiMachine) = machine.Tq0_p
+get_Td0_pp(machine::SauerPaiMachine) = machine.Td0_pp
+get_Tq0_pp(machine::SauerPaiMachine) = machine.Tq0_pp
+get_γ_d1(machine::SauerPaiMachine) = machine.γ_d1
+get_γ_q1(machine::SauerPaiMachine) = machine.γ_q1
+get_γ_d2(machine::SauerPaiMachine) = machine.γ_d2
+get_γ_q2(machine::SauerPaiMachine) = machine.γ_q2
 
 ```
 The module has two functions:
@@ -99,6 +128,19 @@ function initialize_machine(machine::SauerPaiMachine, V_terminal, delta, P, Q)
     ```
     # Calculate initial values for the six states based on steady-state conditions
     # This is a simplified initialization
+    R = get_R(machine)
+    Xd = get_Xd(machine)
+    Xq = get_Xq(machine)
+    Xd_p = get_Xd_p(machine)
+    Xq_p = get_Xq_p(machine)
+    Xd_pp = get_Xd_pp(machine)
+    Xq_pp = get_Xq_pp(machine)
+    Xl = get_Xl(machine)
+    Td0_p = get_Td0_p(machine)
+    γ_d1 = get_γ_d1(machine)
+    γ_q1 = get_γ_q1(machine)
+    γ_d2 = get_γ_d2(machine)
+    γ_q2 = get_γ_q2(machine)
 
     V_mag = abs(V_terminal)
     I_complex = conj((P + im * Q) / V_terminal)
@@ -150,18 +192,60 @@ function initialize_machine(machine::SauerPaiMachine, V_terminal, delta, P, Q)
 
     # Use Milano Eqn. 15.6 to reconstruct the electrical torque
     τ_e = ψ_d * i_q - ψ_q * i_d
-
+    ω0 = 1.0
     # In steady-state, τ_e = τ_m
     τ_m = τ_e
+    V_R = real(V_terminal)
+    V_I = imag(V_terminal)
+    function f!(out, x)
+        δ = x[1]
+        τm = x[2]
+        Vf0 = x[3]
+        ψq = x[4]
+        ψd = x[5]
+        eq_p = x[6]
+        ed_p = x[7]
+        ψd_pp = x[8]
+        ψq_pp = x[9]
 
-    # Prepare state vector for return
-    states = zeros(Float64, 6)
-    states[DELTA] = δ
-    states[OMEGA] = 1.0
-    states[EQ_P] = eq_p
-    states[ED_P] = ed_p
-    states[PSI_D_PP] = ψd_pp
-    states[PSI_Q_PP] = ψq_pp
+        # Milano
+        V_dq = ri_dq_machine(δ) * [V_R; V_I]
+        i_d = (1.0 / Xd_pp) * (γ_d1 * eq_p - ψd + (1 - γ_d1) * ψd_pp)      #15.15
+        i_q = (1.0 / Xq_pp) * (-γ_q1 * ed_p - ψq + (1 - γ_q1) * ψq_pp)     #15.15
+        τ_e = ψd * i_q - ψq * i_d               #15.6
+        out[1] = τm - τ_e #Mechanical Torque
+        out[2] = P - (V_dq[1] * i_d + V_dq[2] * i_q) #Output Power (15.2)
+        out[3] = Q - (V_dq[2] * i_d - V_dq[1] * i_q) #Output Reactive Power (15.3)
+        out[4] = R * i_q - ω0 * ψd + V_dq[2]                                    #15.9 ψq
+        out[5] = R * i_d + ω0 * ψq + V_dq[1]                                    #15.9 ψd
+        out[6] =
+            -eq_p - (Xd - Xd_p) * (i_d - γ_d2 * ψd_pp - (1 - γ_d1) * i_d + γ_d2 * eq_p) +
+            Vf0    #15.13
+        out[7] = -ed_p + (Xq - Xq_p) * (i_q - γ_q2 * ψq_pp - (1 - γ_q1) * i_q - γ_d2 * ed_p)          #15.13
+        out[8] = -ψd_pp + eq_p - (Xd_p - Xl) * i_d      #15.13
+        out[9] = -ψq_pp - ed_p - (Xq_p - Xl) * i_q #15.13
+    end
+
+    x0 = [δ, τ_m, Vf, ψ_q, ψ_d, eq_p, ed_p, ψd_pp, ψq_pp]
+    sol = NLsolve.nlsolve(f!, x0; ftol=STRICT_NLSOLVE_F_TOLERANCE)
+
+    if !NLsolve.converged(sol)
+        @warn("Initialization in machine failed!")
+    else
+        # Get the solution
+        sol_x0 = sol.zero
+
+        states = zeros(Float64, 8)
+        Vf = sol_x0[3]
+        states[DELTA] = δ
+        states[OMEGA] = 1.0
+        states[EQ_P] = eq_p
+        states[ED_P] = ed_p
+        states[PSI_D_PP] = ψd_pp
+        states[PSI_Q_PP] = ψq_pp
+        states[PSI_D] = ψ_d
+        states[PSI_Q] = ψ_q
+    end
 
     # Return initial states and field voltage
     return states, Vf, τ_m
@@ -183,40 +267,22 @@ function update_machine_states!(
     ed_p = states[ED_P]
     ψd_pp = states[PSI_D_PP]
     ψq_pp = states[PSI_Q_PP]
-
+    ψ_d = states[PSI_D]
+    ψ_q = states[PSI_Q]
     # System base values
     f0 = machine.system_base_frequency
 
-    # Extract bus voltage angle and magnitude from complex terminal voltage
-    v_mag = abs(V_terminal)
-    θ = angle(V_terminal)
 
-    # Move from network to machine DQ reference frame
-    # Terminal voltage in dq reference frame
-    v_d = v_mag * sin(δ - θ)       # Eqn 15.4
-    v_q = v_mag * cos(δ - θ)       # Eqn 15.4
+    V_dq = ri_dq_machine(δ) * [real(V_terminal); imag(V_terminal)]
+    v_d = V_dq[1]       # Eqn 15.4
+    v_q = V_dq[2]       # Eqn 15.4
     V_mag = abs(complex(v_d, v_q)) # Debugging
 
-    # Calculate currents and fluxes by solving system of equations from Milano Eq 15.11 and 15.15
-    A = [machine.R 0 0 1;
-        0 machine.R -1 0;
-        machine.Xd_pp 0 1 0;
-        0 machine.Xq_pp 0 1]
-    b = [-v_d;
-        -v_q;
-        machine.γ_d1 * eq_p + (1 - machine.γ_d1) * ψd_pp;
-        -1 * machine.γ_q1 * ed_p + (1 - machine.γ_q1) * ψq_pp]
-    solution = A \ b
-    i_d = solution[1]
-    i_q = solution[2]
-    ψ_d = solution[3]
-    ψ_q = solution[4]
-    # TODO: This is wrong – need to revisit
-    I_mag = abs(complex(i_d, i_q))
-
+    i_d = (-ψ_d + machine.γ_d1 * eq_p + (1 - machine.γ_d1) * ψd_pp) / machine.Xd_pp
+    i_q = (-ψ_q - machine.γ_q1 * ed_p + (1 - machine.γ_q1) * ψq_pp) / machine.Xq_pp
     # Calculate electrical torque
     τ_e = ψ_d * i_q - ψ_q * i_d
-
+    I_mag = abs(complex(i_d, i_q)) # Debugging
 
     # State derivatives
     # shaft equations (15.5 in Milano's book)
@@ -228,7 +294,8 @@ function update_machine_states!(
         τ_m - τ_e - (machine.D * (ω - 1.0))
     )
 
-
+    derivatives[PSI_D] = (machine.R * i_d + ω * ψ_q + v_d) * (2.0 * π * f0)
+    derivatives[PSI_Q] = (machine.R * i_q - ω * ψ_d + v_q) * (2.0 * π * f0)
     # flux equations (15.13 in Milano's book)
     derivatives[EQ_P] = (1.0 / machine.Td0_p) * (
         -eq_p + Vf - (machine.X_d - machine.Xd_p) * (
@@ -239,7 +306,7 @@ function update_machine_states!(
     # Also 15.13 in Milano's book
     derivatives[ED_P] = (1.0 / machine.Tq0_p) * (
         -ed_p + (machine.X_q - machine.Xq_p) * (
-            i_q - machine.γ_q2 * ψq_pp - (1 - machine.γ_q1) * i_q - machine.γ_q2 * ed_p
+            i_q - machine.γ_q2 * ψq_pp - (1 - machine.γ_q1) * i_q - machine.γ_d2 * ed_p
         )
     )
 
